7d9c95c685d2c7d18cae6c249f2ac998c2120739
unknown <samvthom16@gmail.com>
2020-01-16 14:37:59 +0530

    Override logos

---
+++ adfi.php
@@ -0,0 +1,19 @@
+<?php
+	/*
+    Plugin Name: ADFI
+    Plugin URI: http://sputznik.com
+    Description:
+    Author: Samuel Thomas
+    Version: 1.0
+    Author URI: http://sputznik.com
+  */
+
+  add_filter( 'adf_main_logo', function( $url ){
+    $url = plugins_url('adfi/images/logo.png');
+    return $url;
+  } );
+
+  add_filter( 'adf_footer_logo', function( $url ){
+    $url = plugins_url('adfi/images/logo.png');
+    return $url;
+  } );
